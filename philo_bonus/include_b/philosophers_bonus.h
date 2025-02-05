fa7abe46559bee112bb0151c3571c4b177f3b27f
Cristopher Tommasi <ctommasi@student.42madrid.com>
2025-02-05 13:39:21 +0100

    HAHAHA

---
+++ philo_bonus/include_b/philosophers_bonus.h
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers_bonus.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctommasi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 13:19:55 by ctommasi          #+#    #+#             */
+/*   Updated: 2025/01/24 13:19:56 by ctommasi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_BONUS_H
+# define PHILOSOPHERS_BONUS_H
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <string.h>
+# include <sys/time.h>
+
+# define PHILOS_MAX 200
+
+# define ERR_ARGS "Usage:\n  ./philo <number_of_philosopher> <time_to_die> %s"
+# define ERR_ARGS2 "<time_to_eat> <time_to_sleep> [meals_to_have]\n"
+# define ERR_ARGS_VALID "Values must be digits from 0 to UINT_MAX\n"
+# define ERR_ARGS_PHILO_MAX "<number_of_philosopher> cannot exceed 200\n"
+# define ERR_THREAD_CREATE "Failed to create thread.\n"
+# define ERR_THREAD_JOIN "Failed joining threads.\n"
+# define ERR_MUTEX_INIT "Failed to initialize mutex\n"
+# define ERR_TIME "Error getting time\n"
+
+# define MSG_SLEEP "is sleeping\n"
+# define MSG_THINK "is thinking\n"
+# define MSG_EAT "is eating\n"
+# define MSG_FORK "has taken a fork\n"
+# define MSG_DEATH "died\n"
+
+typedef struct s_table
+{
+	pthread_t		table_thread;
+	size_t			philosophers_count;
+	int				someone_died;
+	int				simulation_continues;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	l_fork;
+	pthread_mutex_t	r_fork;
+	pthread_mutex_t	write_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	sim_lock;
+	pthread_mutex_t	sleep_lock;
+	pthread_mutex_t	wait_lock;
+}	t_table;
+
+typedef struct s_philo
+{
+	size_t				id;
+	size_t				time_to_eat;
+	size_t				time_to_die;
+	size_t				time_to_sleep;
+	size_t				meals_to_have;
+	size_t				is_eating;
+	size_t				meals_had;
+	size_t				start_time;
+	size_t				last_meal;
+	pthread_t			thread;
+	t_table				*table;
+	pthread_mutex_t		*l_fork;
+	pthread_mutex_t		*r_fork;
+	pthread_mutex_t		*write_lock;
+	pthread_mutex_t		*meal_lock;
+	pthread_mutex_t		*sim_lock;
+	pthread_mutex_t		*sleep_lock;
+	pthread_mutex_t		*wait_lock;
+}	t_philo;
+
+//-----------------------------------------------INIT
+int		main(int argc, char **argv);
+int		init_table(t_table *table, pthread_mutex_t *forks, char **argv);
+void	init_philos(t_table *table, t_philo *philos, char **argv);
+void	set_forks(t_philo *philo, t_table *table, int i);
+//-----------------------------------------------THREADS
+int		create_threads(t_philo *philos);
+void	join_and_destroy_threads(t_philo *philo);
+//-----------------------------------------------PHILO ROUTINE
+void	*philo_routine(void *param);
+void	eat_and_sleep(t_philo *philo);
+void	precise_think(t_philo *philo);
+void	status_msg(t_philo *philo, size_t *id, char *string);
+void	*one_philosopher(t_philo *philo);
+//-----------------------------------------------TABLE ROUTINE
+void	*table_routine(void *param);
+int		dead_or_finished_eating(t_philo *philo);
+int		anyone_died(t_philo *philo);
+int		simulation_continues(t_philo *philo);
+void	stop_simulation(t_philo *philo);
+//-----------------------------------------------UTILS
+int		check_valid_values(char **argv);
+int		ft_isdigit(int c);
+size_t	get_time(void);
+long	ft_atol(const char *str);
+void	ft_usleep(t_philo *philo, size_t time_to_usleep);
+
+#endif
---
+++ philo_bonus/src_b/init_bonus.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctommasi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 11:37:31 by ctommasi          #+#    #+#             */
+/*   Updated: 2025/01/27 11:37:32 by ctommasi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include_b/philosophers_bonus.h"
+
+int	init_table(t_table *table, pthread_mutex_t *forks, char **argv)
+{
+	size_t	i;
+
+	table->philosophers_count = ft_atol(argv[1]);
+	table->forks = forks;
+	table->simulation_continues = 1;
+	i = 0;
+	while (i < table->philosophers_count)
+	{
+		if (pthread_mutex_init(&table->forks[i], NULL) != 0)
+			return (1);
+		i++;
+	}
+	if (pthread_mutex_init(&table->write_lock, NULL) != 0)
+		return (1);
+	if (pthread_mutex_init(&table->meal_lock, NULL) != 0)
+		return (1);
+	if (pthread_mutex_init(&table->sim_lock, NULL) != 0)
+		return (1);
+	if (pthread_mutex_init(&table->sleep_lock, NULL) != 0)
+		return (1);
+	return (0);
+}
+
+void	init_philos(t_table *table, t_philo *philo, char **argv)
+{
+	size_t		i;
+	size_t		start_time;
+
+	i = 0;
+	start_time = get_time();
+	while (i < table->philosophers_count)
+	{
+		philo[i].id = i;
+		philo[i].time_to_die = ft_atol(argv[2]);
+		philo[i].time_to_eat = ft_atol(argv[3]);
+		philo[i].time_to_sleep = ft_atol(argv[4]);
+		philo[i].meals_to_have = 0;
+		if (argv[5])
+			philo[i].meals_to_have = ft_atol(argv[5]);
+		philo[i].meals_had = 0;
+		philo[i].start_time = start_time;
+		philo[i].last_meal = start_time;
+		philo[i].table = table;
+		set_forks(philo, table, i);
+		philo[i].write_lock = &table->write_lock;
+		philo[i].meal_lock = &table->meal_lock;
+		philo[i].sim_lock = &table->sim_lock;
+		philo[i].sleep_lock = &table->sleep_lock;
+		i++;
+	}
+}
+
+void	set_forks(t_philo *philo, t_table *table, int i)
+{
+	if (philo[i].id % 2 == 0)
+	{
+		philo[i].l_fork = &table->forks[i];
+		philo[i].r_fork = &table->forks[(i + 1)
+			% philo->table->philosophers_count];
+	}
+	else if (philo[i].id % 2 != 0)
+	{
+		philo[i].l_fork = &table->forks[(i + 1)
+			% philo->table->philosophers_count];
+		philo[i].r_fork = &table->forks[i];
+	}
+}
